e18cf8cfe56d164774a710fdac876d69df3708ab
VlatkovicB <vlatkovicbojan@gmail.com>
2022-01-10 11:55:32 +0100

    finish day five, part two

--- day-05/index.js
+++ day-05/index.js
@@ -1,7 +1,8 @@
 import { data, sample } from "./utils.js";
 
 const checkHorizontalAndVertical = p => p[0][0] === p[1][0] || p[0][1] === p[1][1]
-const checkDiagonal = (p) => (p[0][0] === p[0][1] && p [1][0] === p[1][1]) || (p[0][0] === p[1][1] && p[1][0] === p[0][1])
+const applyFilter = (data) => data.filter(line => (line[0][0] === line[1][0] || line[0][1] === line[1][1]) && line)
+const checkDiagonal = (p) => (p[0][0] === p[0][1] && p [1][0] === p[1][1]) || (p[0][0] === p[1][1] && p[1][0] === p[0][1]) || (Math.abs(p[0][0] - p[1][0]) === Math.abs(p[0][1] - p[1][1]))
 const generateMatrix = (x, y) => Array(Number(x)).fill(undefined).map(() => Array(Number(y)).fill(0))
 const determineSize = (d) => d.reduce((p, c) => {
   if (c[0][0] > p.x) p.x = c[0][0]
@@ -16,11 +17,13 @@ const changeValue = (value, directive) => {
   if (directive === '-') return value - 1
   return value
 }
+
 const check = (sign, n, endN) => {
   if (sign === '+') return n <= endN
   if (sign === '-') return n >= endN
   return true
 }
+
 const markNodes = (data, points) => {
   const x1 = points[0][0]
   const x2 = points[1][0]
@@ -30,7 +33,7 @@ const markNodes = (data, points) => {
   const ySign = y1 > y2 ? "-" : y2 > y1 ? "+" : ""
 
   if (checkDiagonal(points) || checkHorizontalAndVertical(points)) {
-    for (let x = x1, y = y1, endX = x2, endY = y2; check(xSign, x, endX) && check(ySign, y, endY);) {
+    for (let x = x1, y = y1; check(xSign, x, x2) && check(ySign, y, y2);) {
       data[x][y] = data[x][y] + 1
       x = changeValue(x, xSign)
       y = changeValue(y, ySign)
@@ -38,23 +41,19 @@ const markNodes = (data, points) => {
   }
 }
 
-const getOverlapping = (data) => {
+const getOverlapping = (data, diagonal = false) => {
+  if (!diagonal) data = applyFilter(data)
   const { x, y } = determineSize(data)
   const emptyMatrix = generateMatrix(x + 1, y + 1)
-  data.forEach(points => {
-    markNodes(emptyMatrix, points)
-  })
+  data.forEach(points => markNodes(emptyMatrix, points))
 
-  if (data.length < 20) console.table(emptyMatrix)
-
-  console.log(emptyMatrix.length)
   return emptyMatrix.reduce((prev, curr) => {
     const number = curr.filter(n => n > 1).length
     return prev + number
   }, 0)
 }
 
-// const noDiagonal = getOverlapping(sample)
-// console.log(noDiagonal)
-const diagonal = getOverlapping(data)
-console.log(diagonal)
+const noDiagonal = getOverlapping(data)
+console.log(noDiagonal) // SOLUTION 6548
+const diagonal = getOverlapping(data, true)
+console.log(diagonal) // SOLUTION 19663
