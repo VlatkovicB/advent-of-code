dc546fbf5c1a48823f8cd4bcf15357286f3fa3e6
VlatkovicB <vlatkovicbojan@gmail.com>
2022-01-10 14:56:10 +0100

    finish day six, part two

--- day-06/index.js
+++ day-06/index.js
@@ -1,3 +1,38 @@
-import { data } from "./utils.js";
+import { data, sample } from "./utils.js";
 
-console.log(data)
+const calculateNumberOfFish = (data, days) => {
+  const fishBuckets = Array(9).fill(0)
+  data.forEach(d => fishBuckets[d]++)
+
+  for (let i = 0; i < days; i++) {
+    const numberOfFishes = fishBuckets.shift()
+    fishBuckets.push(numberOfFishes)
+    fishBuckets[6] += numberOfFishes
+  }
+
+  return fishBuckets.reduce((p, c) => p + c, 0)
+  /**
+   *
+   let fishes = data.join('')
+   for (let i = 0; i < days; i++) {
+    let addToQueue = ''
+    fishes = fishes.split('').map(fish => {
+      if (fish - 1 < 0) {
+        addToQueue += 8
+        return 6
+      }
+      return fish - 1
+    }).join('')
+    fishes += addToQueue
+  }
+
+   return fishes.length */
+}
+
+const dayOne = calculateNumberOfFish(data, 80) // SOLUTION 383160
+const dayTwo = calculateNumberOfFish(data, 256) // SOLUTION 383160
+const sampleResult = calculateNumberOfFish(sample, 18)
+
+console.log(dayOne)
+console.log(dayTwo)
+console.log(sampleResult)
